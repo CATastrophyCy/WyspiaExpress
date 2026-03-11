d2e61bbdbca7b04a6375d43624aac1a1070c2855
CATastrophyCy <96376365+CATastrophyCy@users.noreply.github.com>
2026-03-11 20:45:17 +0100

    Added tooltip rendering for fake gun

--- src/client/java/org/cat/express/wyspiaexpress/client/WyspiaexpressClient.java
+++ src/client/java/org/cat/express/wyspiaexpress/client/WyspiaexpressClient.java
@@ -1,10 +1,20 @@
 package org.cat.express.wyspiaexpress.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
+import org.cat.express.wyspiaexpress.WyspiaExpressItems;
+import org.cat.express.wyspiaexpress.client.items.ItemToolTip;
 
 public class WyspiaexpressClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+        registerItemToolTips();
+    }
+
+    public static void registerItemToolTips(){
+        ItemTooltipCallback.EVENT.register(((itemStack, tooltipContext, tooltipType, list) -> {
+            ItemToolTip.addItemtip(WyspiaExpressItems.FAKE_REVOLVER, itemStack, list);
+        }));
     }
 }
