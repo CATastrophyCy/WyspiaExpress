86f286ed5b7f48681310594ef3464abde8dc5ab8
CATastrophyCy <96376365+CATastrophyCy@users.noreply.github.com>
2026-03-11 18:47:15 +0100

    Added code to properly register an item and its cooldown

--- src/main/java/org/cat/express/wyspiaexpress/WyspiaExpressItems.java
+++ src/main/java/org/cat/express/wyspiaexpress/WyspiaExpressItems.java
@@ -2,17 +2,27 @@ package org.cat.express.wyspiaexpress;
 
 import dev.doctor4t.wathe.api.event.AllowPlayerPunching;
 import dev.doctor4t.wathe.api.event.ShouldDropOnDeath;
+import dev.doctor4t.wathe.game.GameConstants;
+import dev.doctor4t.wathe.game.GameFunctions;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.util.Hand;
+import net.minecraft.util.Identifier;
 import org.cat.express.wyspiaexpress.config.WyspiaExpressServerConfig;
 import org.cat.express.wyspiaexpress.shop.EnumShopEntry;
 import org.cat.express.wyspiaexpress.shop.ShopUtil;
-
+import org.jetbrains.annotations.NotNull;
 import java.util.HashMap;
 
 public class WyspiaExpressItems {
 
     public static final HashMap<Item, WyspiaExpressServerConfig.ItemBasicConfig> ITEMS_BASIC_CONFIG = new HashMap<>();
+
     public static void init(){
         registerItemConfig();
 
@@ -22,7 +32,25 @@ public class WyspiaExpressItems {
         ITEMS_BASIC_CONFIG.put(ShopUtil.fromEnumShopEntry(EnumShopEntry.FAKE_KNIFE).getItem(), WyspiaExpress.CONFIG.itemConfig.fakeKnifeConfig.basic);
         ITEMS_BASIC_CONFIG.put(ShopUtil.fromEnumShopEntry(EnumShopEntry.MEDICAL_KIT).getItem(), WyspiaExpress.CONFIG.itemConfig.medicalKitConfig.basic);
     }
+    public static Item registerItem(Item item, String id) {
+        Identifier itemID = Identifier.of(WyspiaExpress.MOD_ID, id);
 
+        return Registry.register(Registries.ITEM, itemID, item);
+    }
+    public static void setItemCooldown(@NotNull PlayerEntity player, @NotNull Item item, Hand hand) {
+        if (GameFunctions.isPlayerAliveAndSurvival(player)) {
+            player.getItemCooldownManager().set(item, GameConstants.ITEM_COOLDOWNS.get(item));
+            if (hand != null) player.getStackInHand(hand).decrement(1);
+        }
+    }
+    public static void registerItemCooldown(Item item, int minutes, int seconds) {
+        GameConstants.ITEM_COOLDOWNS.put(item, GameConstants.getInTicks(minutes, seconds));
+    }
+    public static void registerItemGroup(Item item, RegistryKey<ItemGroup> itemGroup){
+        ItemGroupEvents.modifyEntriesEvent(itemGroup).register( entries -> {
+            entries.add(item);
+        });
+    }
     public static void registerEvents(){
         registerPunchEvent();
         registerDropEvent();
