9b00dff74219e5f56284d28e8ce4c145bc8357c8
CATastrophyCy <96376365+CATastrophyCy@users.noreply.github.com>
2026-03-13 00:17:02 +0100

    chore: remove itemBasicConfig comments

--- src/main/java/org/cat/express/wyspiaexpress/config/ItemBasicConfig.java
+++ src/main/java/org/cat/express/wyspiaexpress/config/ItemBasicConfig.java
@@ -3,10 +3,7 @@ package org.cat.express.wyspiaexpress.config;
 import blue.endless.jankson.Comment;
 import io.wispforest.owo.config.annotation.RangeConstraint;
 public class ItemBasicConfig {
-    @Comment("Render Item on hand?")
     public boolean renderItemOnHand = true;
-    @Comment("Can punch players")
     public boolean canPunchPlayers = false;
-    @Comment("Should drop on death")
     public boolean dropOnDeath = false;
 }
