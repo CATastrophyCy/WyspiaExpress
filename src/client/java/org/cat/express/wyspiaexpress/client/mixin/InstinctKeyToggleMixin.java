0379f20178e13a8c834cec2d4a60184c2f498c31
CAT <96376365+CATastrophyCy@users.noreply.github.com>
2026-05-09 13:55:12 +0200

    Instinct change

--- src/client/java/org/cat/express/wyspiaexpress/client/mixin/InstinctKeyToggleMixin.java
+++ src/client/java/org/cat/express/wyspiaexpress/client/mixin/InstinctKeyToggleMixin.java
@@ -12,20 +12,23 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(KeyBinding.class)
 public abstract class InstinctKeyToggleMixin {
 
-    @Shadow private int timesPressed;
+    @Shadow private boolean pressed;
 
     @Unique
     private static boolean toggle = false;
 
+    @Unique
+    private boolean wasDown = false;
+
     @Inject(method = "isPressed", at = @At("HEAD"), cancellable = true)
     private void wyspiaexpress$onIsPressed(CallbackInfoReturnable<Boolean> cir) {
         if ((Object) this == WatheClient.instinctKeybind) {
-            // Consume the vanilla press counter to flip the toggle state.
-            // If the key was pressed quickly multiple times, it processes all of them.
-            while (this.timesPressed > 0) {
+            boolean isDown = this.pressed;
+
+            if (isDown && !wasDown) {
                 toggle = !toggle;
-                this.timesPressed--;
             }
+            wasDown = isDown;
             cir.setReturnValue(toggle);
         }
     }
--- src/client/java/org/cat/express/wyspiaexpress/client/mixin/InstinctMixin.java
+++ src/client/java/org/cat/express/wyspiaexpress/client/mixin/InstinctMixin.java
@@ -1,48 +1,81 @@
 package org.cat.express.wyspiaexpress.client.mixin;
 
+import dev.doctor4t.wathe.cca.GameWorldComponent;
 import dev.doctor4t.wathe.cca.PlayerPoisonComponent;
 import dev.doctor4t.wathe.client.WatheClient;
 import dev.doctor4t.wathe.game.GameFunctions;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import org.BsXinQin.kinswathe.KinsWatheRoles;
 import org.BsXinQin.kinswathe.roles.dreamer.DreamerComponent;
 import org.BsXinQin.kinswathe.roles.physician.PhysicianComponent;
+import org.agmas.noellesroles.Noellesroles;
 import org.agmas.noellesroles.bartender.BartenderPlayerComponent;
+import org.agmas.noellesroles.morphling.MorphlingPlayerComponent;
+import org.aussiebox.starexpress.StarryExpressRoles;
+import org.aussiebox.starexpress.cca.StarstruckComponent;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.awt.*;
+import java.util.UUID;
 
 @Mixin(WatheClient.class)
 public abstract class InstinctMixin {
+    // apparently they replaced fake poison's poisoner to this uuid, and in PlayerPoisonComponent they used a mixin to guarantee that delusion_maker won't kill
+    @Unique private static final UUID DELUSION_MARKER = UUID.fromString("00000000-0000-0000-dead-c0de00000000"); // unique string used by Kinswathe
 
     @Inject(method = "getInstinctHighlight", at = @At("HEAD"), cancellable = true)
     private static void getInstinctHighlightColor(Entity target, CallbackInfoReturnable<Integer> cir) {
 
         if (target instanceof PlayerEntity targetPlayer) {
             if(WatheClient.isPlayerSpectatingOrCreative() && GameFunctions.isPlayerAliveAndSurvival(targetPlayer) && WatheClient.isInstinctEnabled() ) {
-                    PlayerPoisonComponent playerPoisonComponent = PlayerPoisonComponent.KEY.get(targetPlayer);
-                    BartenderPlayerComponent bartenderPlayerComponent = BartenderPlayerComponent.KEY.get(targetPlayer);
-                    PhysicianComponent physicianComponent = PhysicianComponent.KEY.get(targetPlayer);
-                    DreamerComponent dreamerComponent = DreamerComponent.KEY.get(targetPlayer);
-                    if (playerPoisonComponent.poisonTicks > 0) {
-                        cir.setReturnValue(Color.RED.getRGB());
+                GameWorldComponent gameWorldComponent = GameWorldComponent.KEY.get(targetPlayer);
+                PlayerPoisonComponent playerPoisonComponent = PlayerPoisonComponent.KEY.get(targetPlayer);
+                BartenderPlayerComponent bartenderPlayerComponent = BartenderPlayerComponent.KEY.get(targetPlayer);
+                PhysicianComponent physicianComponent = PhysicianComponent.KEY.get(targetPlayer);
+                StarstruckComponent starstruckComponent = StarstruckComponent.KEY.get(targetPlayer);
+                DreamerComponent dreamerComponent = DreamerComponent.KEY.get(targetPlayer);
+                MorphlingPlayerComponent morphlingPlayerComponent = MorphlingPlayerComponent.KEY.get(targetPlayer);
+                if( starstruckComponent.ticks > 0 && gameWorldComponent.isRole(targetPlayer, StarryExpressRoles.STARSTRUCK)) {
+                    cir.setReturnValue(0x77C2F2);
+                    cir.cancel();
+                    return;
+                }
+                if( gameWorldComponent.isRole(targetPlayer, KinsWatheRoles.ROBOT) && targetPlayer.hasStatusEffect(StatusEffects.NIGHT_VISION)) {
+                    cir.setReturnValue(0xE0E0E0);
+                    cir.cancel();
+                    return;
+                }
+                if( gameWorldComponent.isRole(targetPlayer, Noellesroles.MORPHLING) && morphlingPlayerComponent.morphTicks > 0 ) {
+                    cir.setReturnValue(0x2E0000);
+                    cir.cancel();
+                    return;
+                }
+                if (playerPoisonComponent.poisonTicks > 0) {
+                    if(playerPoisonComponent.poisoner.equals(DELUSION_MARKER)) {
+                        cir.setReturnValue(0xB099FF);
                         cir.cancel();
                         return;
                     }
-                    if( bartenderPlayerComponent.armor > 0 || physicianComponent.physicianArmor > 0) {
-                        cir.setReturnValue(Color.BLUE.getRGB());
-                        cir.cancel();
-                        return;
-                    }
-                    if( dreamerComponent.dreamArmor > 0) {
-                        cir.setReturnValue(KinsWatheRoles.DREAMER.color());
-                        cir.cancel();
-                        return;
-                    }
+                    cir.setReturnValue(Color.RED.getRGB());
+                    cir.cancel();
+                    return;
+                }
+                if( bartenderPlayerComponent.armor > 0 || physicianComponent.physicianArmor > 0) {
+                    cir.setReturnValue(Color.BLUE.getRGB());
+                    cir.cancel();
+                    return;
+                }
+                if( dreamerComponent.dreamArmor > 0) {
+                    cir.setReturnValue(0xFF6BFA);
+                    cir.cancel();
+                    return;
+                }
             }
         }
     }
