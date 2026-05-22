8506d8bd02059a979ff4e1df256258044463bf39
CAT <96376365+CATastrophyCy@users.noreply.github.com>
2026-05-22 12:27:25 +0200

    Fix thief not using the actual config

--- src/main/java/org/cat/express/wyspiaexpress/mixin/roles/ThiefPacketC2SMixin.java
+++ src/main/java/org/cat/express/wyspiaexpress/mixin/roles/ThiefPacketC2SMixin.java
@@ -1,6 +1,7 @@
 package org.cat.express.wyspiaexpress.mixin.roles;
 
 import dev.doctor4t.wathe.game.GameConstants;
+import org.cat.express.wyspiaexpress.WyspiaExpress;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
@@ -10,22 +11,22 @@ import pro.fazeclan.river.stupid_express.role.thief.packet.ThiefTakeItemC2SPacke
 public abstract class ThiefPacketC2SMixin {
     @ModifyConstant(method = "handleThiefTakeItem", constant = @Constant(intValue = 900))
     private static int modifyFailedCooldown(int original) {
-        return GameConstants.getInTicks(0, 30); // e.g., 400
+        return GameConstants.getInTicks(0, WyspiaExpress.ROLES_CONFIG.roleConfig.stupidExpress.thiefConfig.failCooldown());
     }
 
     @ModifyConstant(method = "handleThiefTakeItem", constant = @Constant(intValue = 1800))
     private static int modifySuccessCooldown(int original) {
-        return GameConstants.getInTicks(0, 60); // e.g., 1200
+        return GameConstants.getInTicks(0, WyspiaExpress.ROLES_CONFIG.roleConfig.stupidExpress.thiefConfig.cooldown()); // e.g., 1200
     }
 
     @ModifyConstant(method = "validateDistance", constant = @Constant(doubleValue = 1.0F))
     private static double modifyClientDistance(double original) {
-        return 1.5;
+        return WyspiaExpress.ROLES_CONFIG.roleConfig.stupidExpress.thiefConfig.clientDistance();
     }
 
     @ModifyConstant(method = "validateDistance", constant = @Constant(doubleValue = 1.2))
     private static double modifyServerDistance(double original) {
-        return 1.5; // e.g., 3.0
+        return WyspiaExpress.ROLES_CONFIG.roleConfig.stupidExpress.thiefConfig.serverDistance();
     }
 }
 
