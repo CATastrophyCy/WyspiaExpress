5d572e5360b544ae49f1250b2997fa9963e78c97
CAT <96376365+CATastrophyCy@users.noreply.github.com>
2026-07-07 09:42:31 +0200

    Phantom ability will now remove glowing

--- src/main/java/org/cat/express/wyspiaexpress/mixin/noellesroles/NoellesRolesMixin.java
+++ src/main/java/org/cat/express/wyspiaexpress/mixin/noellesroles/NoellesRolesMixin.java
@@ -1,7 +1,10 @@
 package org.cat.express.wyspiaexpress.mixin.noellesroles;
 
 import dev.doctor4t.wathe.game.GameConstants;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.server.network.ServerPlayerEntity;
 import org.agmas.noellesroles.AbilityPlayerComponent;
 import org.agmas.noellesroles.Noellesroles;
 import org.cat.express.wyspiaexpress.WyspiaExpress;
@@ -16,26 +19,23 @@ public abstract class NoellesRolesMixin {
     @Redirect(
             method = "lambda$registerPackets$16",
             at = @At(
-                    value = "NEW",
-                    target = "net/minecraft/entity/effect/StatusEffectInstance"
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/server/network/ServerPlayerEntity;addStatusEffect(Lnet/minecraft/entity/effect/StatusEffectInstance;)Z"
             )
     )
-    private static StatusEffectInstance wyspiaexpress$phantomDuration(
-            net.minecraft.registry.entry.RegistryEntry<net.minecraft.entity.effect.StatusEffect> effect,
-            int duration,
-            int amplifier,
-            boolean ambient,
-            boolean showParticles,
-            boolean showIcon
-    ) {
-        return new StatusEffectInstance(
-                effect,
-                GameConstants.getInTicks(0,WyspiaExpress.ROLES_CONFIG.roleConfig.noellesRoles.phantomConfig.duration()),
-                amplifier,
-                ambient,
-                showParticles,
-                showIcon
+    private static boolean wyspiaexpress$onPhantomAbility(ServerPlayerEntity instance, StatusEffectInstance statusEffectInstance) {
+        instance.removeStatusEffect(StatusEffects.GLOWING);
+
+        StatusEffectInstance modifiedEffect = new StatusEffectInstance(
+                statusEffectInstance.getEffectType(),
+                GameConstants.getInTicks(0, WyspiaExpress.ROLES_CONFIG.roleConfig.noellesRoles.phantomConfig.duration()),
+                statusEffectInstance.getAmplifier(),
+                statusEffectInstance.isAmbient(),
+                statusEffectInstance.shouldShowParticles(),
+                statusEffectInstance.shouldShowIcon()
         );
+
+        return instance.addStatusEffect(modifiedEffect);
     }
     @Redirect(
             method = "lambda$registerPackets$16",
