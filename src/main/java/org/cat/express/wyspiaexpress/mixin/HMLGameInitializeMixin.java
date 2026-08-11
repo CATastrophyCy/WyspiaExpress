a6619e0628e97bd2f341e6cfc644830f93fb3b4a
CAT <96376365+CATastrophyCy@users.noreply.github.com>
2026-08-11 18:44:39 +0200

    fix: fixed ForceRole not working on neutral roles, note that it will no longer bypass player requirement

--- src/main/java/org/cat/express/wyspiaexpress/mixin/HMLGameInitializeMixin.java
+++ src/main/java/org/cat/express/wyspiaexpress/mixin/HMLGameInitializeMixin.java
@@ -109,7 +109,7 @@ public abstract class HMLGameInitializeMixin {
             }
         }
         // lich initialize
-        if (!HarpyModLoaderConfig.HANDLER.instance().disabled.contains(WyspiaExpressRoles.LICH.identifier().toString())) {
+        if (!HarpyModLoaderConfig.HANDLER.instance().disabled.contains(WyspiaExpressRoles.getRoleId(WyspiaExpressRoles.LICH))) {
             int maxRevives = WyspiaExpress.ROLES_CONFIG.roleConfig.lichConfig.additionalRevive() + killerCount;
             LichReviveComponent component = LichReviveComponent.KEY.get(serverWorld);
             component.setAvailableRevives(WyspiaExpress.ROLES_CONFIG.roleConfig.lichConfig.additionalRevive());
@@ -387,7 +387,7 @@ public abstract class HMLGameInitializeMixin {
         for (Role role : WatheRoles.ROLES) {
             if (Harpymodloader.NON_MURDER_ROLES.contains(role)) continue;
             if (Harpymodloader.VANNILA_ROLES.contains(role)) continue;
-            if (HarpyModLoaderConfig.HANDLER.instance().disabled.contains(role.identifier().toString())) continue;
+            if (HarpyModLoaderConfig.HANDLER.instance().disabled.contains(WyspiaExpressRoles.getRoleId(role))) continue;
             if(!WyspiaExpressRoles.roleMeetPlayerRequirement(role)) continue;
             if (role.canUseKiller()) {
                 killerRoles.add(role);
@@ -423,7 +423,7 @@ public abstract class HMLGameInitializeMixin {
         for (Role role : WatheRoles.ROLES) {
             if (Harpymodloader.NON_MURDER_ROLES.contains(role)) continue;
             if (Harpymodloader.VANNILA_ROLES.contains(role)) continue;
-            if (HarpyModLoaderConfig.HANDLER.instance().disabled.contains(role.identifier().toString())) continue;
+            if (HarpyModLoaderConfig.HANDLER.instance().disabled.contains(WyspiaExpressRoles.getRoleId(role))) continue;
             if(!WyspiaExpressRoles.roleMeetPlayerRequirement(role)) continue;
             if (WyspiaExpressRoles.TRUE_NEUTRALS.contains(role)) {
                 trueNeutralRoles.add(role);
@@ -487,7 +487,7 @@ public abstract class HMLGameInitializeMixin {
                 ServerPlayerEntity player = world.getServer().getPlayerManager().getPlayer(uuid);
                 if (player == null) continue;
                 if (!source.contains(player)) continue;
-                if (Harpymodloader.FORCED_MODDED_ROLE_FLIP.containsKey(player.getUuid())) continue;
+                if (!Harpymodloader.FORCED_MODDED_ROLE_FLIP.containsKey(player.getUuid())) continue;
                 if (!Harpymodloader.OVERWRITE_ROLES.contains(gwc.getRole(player))) continue;
 
                 if (pickedUuids.add(player.getUuid())) {
@@ -755,7 +755,7 @@ public abstract class HMLGameInitializeMixin {
 
         List<Modifier> randomModsForPlayer = new ArrayList<>(
                 HMLModifiers.MODIFIERS.stream()
-                        .filter(mod -> !HarpyModLoaderConfig.HANDLER.instance().disabledModifiers.contains(mod.identifier.toString()))
+                        .filter(mod -> !HarpyModLoaderConfig.HANDLER.instance().disabledModifiers.contains(WyspiaExpressRoles.getModifierId(mod)))
                         .toList()
         );
 
